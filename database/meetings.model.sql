9a264385624c370e9c359fd1ed3479acbeff2c1c
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-06-24 15:19:44 +0800

    feat: create meetings table

---
+++ database/meetings.model.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS meetings (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    meeting_date TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
