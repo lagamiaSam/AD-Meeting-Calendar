060cae47481398301111872c9c0732d30a60799e
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-06-24 15:15:47 +0800

    refactor: simplify database connection check by removing unused parts

--- index.php
+++ index.php
@@ -4,8 +4,6 @@ echo "<h2>Database Connection Check</h2>";
 require_once 'bootstrap.php';
 
 // Define HANDLERS_PATH constant
-define('HANDLERS_PATH', __DIR__ . '/handlers/');
-
 require_once HANDLERS_PATH . 'mongodbChecker.handler.php';
 require_once HANDLERS_PATH . 'postgreChecker.handler.php';
 ?>
