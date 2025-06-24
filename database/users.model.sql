7538f25016b59b02778ccff81f680ec741596d26
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-06-24 15:22:26 +0800

    feat: add users table

---
+++ database/users.model.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    full_name VARCHAR(150),
+    group_name VARCHAR(100),
+    role VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
