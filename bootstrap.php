ffff0a58996e6bf3d25f36c76ff6d3bca45c61d1
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-06-24 15:13:08 +0800

    refactor: update to .env and added the informations

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,7 @@
 <?php
 define('BASE_PATH', realpath(__DIR__));
+define('UTILS_PATH', BASE_PATH . '/utils/');
+define('VENDOR_PATH', BASE_PATH . '/vendor/');
+define('HANDLERS_PATH', BASE_PATH . '/handlers/');
 
 chdir(BASE_PATH);
