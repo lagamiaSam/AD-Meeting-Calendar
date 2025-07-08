0dde1cd6927e6fb889916f4ef5639386aec44142
lagamiaSam <167587359+lagamiaSam@users.noreply.github.com>
2025-07-08 20:59:20 +0800

    feat: added dummies path

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,6 @@ define('BASE_PATH', realpath(__DIR__));
 define('UTILS_PATH', BASE_PATH . '/utils/');
 define('VENDOR_PATH', BASE_PATH . '/vendor/');
 define('HANDLERS_PATH', BASE_PATH . '/handlers/');
+define('DUMMIES_PATH', BASE_PATH . '/staticDatas/dummies/'); // ✅ Added for seeding
 
 chdir(BASE_PATH);
